e311ce9e4dbd26f849942fb308fc9550210d6d17
sharathbc88 <sharath.basappachandranna@my.jcu.edu.au>
2016-11-21 12:50:54 +0800

    added salesbonus

--- EnhancedSales.py
+++ EnhancedSales.py
@@ -5,11 +5,14 @@ and prints the bonus until they enter a negative number. """
 MENU = "What is the sales amount ? to display your Bonus"
 print (MENU)
 sales = float(input("Enter Sales: $"))
-if sales < 1000:
-    bonus = sales * 0.1
-    print("Bonus: $",(bonus))
+if sales >=0:
+    if sales >= 0 and sales < 1000:
+        bonus = sales * 0.1
+        print("Bonus: $",(bonus))
+        sales = float(input("Enter Sales: $"))
+    elif sales >= 1000:
+        bonus = sales * 0.15
+        print("Bonus: $",(bonus))
+        sales = float(input("Enter Sales: $"))
 else:
-    bonus = sales * 0.15
-    print("Bonus: $",(bonus))
-else:
-    print("Enter valid sales amount:")
+    exit()
--- SalesBonus.py
+++ SalesBonus.py
@@ -1,13 +1,12 @@
-"""
-Program to calculate and display a user's bonus based on sales.
+
+""" Program to calculate and display a user's bonus based on sales.
 If sales are under $1,000, the user gets a 10% bonus.
-If sales are $1,000 or over, the bonus is 15%.
-"""
+If sales are $1,000 or over, the bonus is 15%."""
 
 MENU = "What is the sales amount ? to display your Bonus"
 print (MENU)
 sales = float(input("Enter Sales: $"))
-if sales < 1000:
+if sales >= 0 and sales < 1000:
     bonus = sales * 0.1
     print("Bonus: $",(bonus))
 elif sales >= 1000:
