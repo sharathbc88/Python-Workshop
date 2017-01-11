ab618c16a05144fd0a8155b57c915890b9509cbc
sharathbc88 <sharath.basappachandranna@my.jcu.edu.au>
2017-01-11 13:56:46 +0800

    practicals 4

--- Practical07/ProgrammingLanguage.py
+++ Practical07/ProgrammingLanguage.py
@@ -1,9 +1,16 @@
 class ProgrammingLanguage:
-    def __init__(self,name, typing, reflection,year):
-        self.name = name
+    def __init__(self,language, typing, reflection,year):
+        self.language = language
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            return True
+        else:
+            return False
+
+
     def __str__(self):
-        return  "{}, {}, Reflection={},First appeared in {}".format(self.name,self.typing,self.reflection,self.year)
+        return "{}, {}, Reflection={},First appeared in {}".format(self.language,self.typing,self.reflection,self.year)
--- Practical07/languages.py
+++ Practical07/languages.py
@@ -4,6 +4,13 @@ def details():
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     vb = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    language_list = [ruby, python, vb]
+    language = [ruby, python, vb]
+    print(ruby)
+    print(python)
+    print(vb)
+    print("Languages that are Dynamic")
+    for self in language:
+        if self.is_dynamic():
+            print(self.language)
 
 details()
